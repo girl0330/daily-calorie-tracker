d5500329ef45900666991eb57b6915df783012c9
phg <girls0330@naver.com>
2026-08-20 17:15:50 +0900

    refactor: 반응형 레이아웃 및 Nav 구조 개선

--- react-app/src/components/common/Nav.tsx
+++ react-app/src/components/common/Nav.tsx
@@ -16,14 +16,19 @@ export default function Nav() {
   };
   return (
     <>
-      <aside className="flex w-70 shrink-0 flex-col bg-(--bg-section) p-5">
-        <img src="/logo.svg" alt="Logo" className="h-20" />
-        {/* <div className="flex h-20 items-center justify-center text-3xl font-bold text-(--primary-3)">Daily Tracker</div> */}
+      // Mobile
+      <header className="flex h-16 w-full items-center justify-between bg-(--bg-section) px-5 lg:hidden">
+        <img src="/logo.svg" alt="Daily Tracker" className="h-10" />
 
-        <div className="my-4 border border-(--primary-1)" />
+        <button type="button" className="cursor-pointer">
+          메뉴
+        </button>
+      </header>
+      // Desktop
+      <div className="hidden w-70 shrink-0 flex-col bg-(--bg-section) p-5 lg:flex">
+        <img src="/logo.svg" alt="Daily Tracker" className="h-10 lg:h-20" />
 
-        <nav className="flex flex-1 flex-col gap-2">
-          <div className="mb-2 text-sm text-(--text-primary) opacity-80">로그인한 유저: {userFromStore?.email}</div>
+        <nav className="mt-8 flex flex-1 flex-col gap-2">
           <NavLink
             to="/"
             className={({ isActive }) =>
@@ -36,6 +41,7 @@ export default function Nav() {
           >
             Daily Tracker
           </NavLink>
+
           <NavLink
             to="/monthly"
             className={({ isActive }) =>
@@ -51,6 +57,8 @@ export default function Nav() {
         </nav>
 
         <div className="mt-auto flex flex-col gap-2 pt-4">
+          <div className="px-4 text-sm text-(--text-primary) opacity-80">{userFromStore?.email}</div>
+
           <NavLink
             to="/reset-password"
             className={({ isActive }) =>
@@ -63,11 +71,15 @@ export default function Nav() {
           >
             비밀번호 재설정
           </NavLink>
-          <button className="cursor-pointer text-left text-sm text-red-500 hover:underline" onClick={handleLogout}>
+
+          <button
+            className="cursor-pointer px-4 py-2 text-left text-sm text-red-500 hover:underline"
+            onClick={handleLogout}
+          >
             로그아웃
           </button>
         </div>
-      </aside>
+      </div>
     </>
   );
 }
--- react-app/src/layout/AppLayout.tsx
+++ react-app/src/layout/AppLayout.tsx
@@ -3,10 +3,10 @@ import Nav from '../components/common/Nav';
 
 export default function AppLayout() {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen flex-col lg:flex-row">
       <Nav />
 
-      <main className="min-w-0 flex-1">
+      <main className="min-w-0 flex-1 p-5">
         <Outlet />
       </main>
     </div>
